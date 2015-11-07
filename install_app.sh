26c0d19aaee01347db2f672ec6e571082cd7a43f
David Regla <dreglad@gmail.com>
2015-11-07 03:16:38 -0600

    adding supervisor

--- install_app.sh
+++ install_app.sh
@@ -9,24 +9,32 @@ mkdir /storage/temp && chmod 777 /storage/temp
 mkdir /storage/temp/status && chmod 777 /storage/temp/status
 mkdir /storage/sprites && chmod 777 /storage/sprites
 mkdir /storage/hls && chmod 777 /storage/hls
+mkdir /var/log/video && chmod 777 /var/log/video
 
 # Create database
 echo "CREATE DATABASE video" | mysql -u root -ppass
 
 # Prepare env
 cd /vagrant/src
-virtualenv --no-site-packages --python /usr/bin/python-2.7 .
+rm -rf bin include local lib
+virtualenv --no-site-packages --python /usr/bin/python2.7 .
 source ./bin/activate
 
 # Install python dependencies
 pip install -r requirements.txt
 
 # Data and migrations
-./manage.py makemigrations
 ./manage.py migrate
 ./manage.py loaddata users tipo_clips tipo_programas categorias paises programas corresponsales servicios
 
 ./manage.py collectstatic --noinput
+
+# django supervisor upstart
+cp /vagrant/etc/etc--init--django-supervisor.conf /etc/init/django-supervisor.conf
+cp /vagrant/etc/etc--init--workaround-vagrant-bug-6074.conf /etc/init/workaround-vagrant-bug-6074.conf
+initctl reload-configuration
+start workaround-vagrant-bug-6074.conf
+start django-supervisor
 
 # Configure web server
 cp /vagrant/etc/apache.video.conf /etc/apache2/sites-available/video.conf
--- src/video/settings.py
+++ src/video/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'djsupervisor',
     'rest_framework',
     'rest_framework_swagger',
     'django_rq',
