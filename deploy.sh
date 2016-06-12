b1fa3621e9451750ae93f453f310f857da881ad4
David Regla <dreglad@gmail.com>
2016-06-12 09:54:54 -0500

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
+# rsync to stging server (La Jornada)
 
-#rsync -n --delete -avz --exclude "vcms/storage" --exclude "*.pyc" --exclude "*.mp4" --exclude "src/bin" . root@64.31.10.168:/vagrant
+rsync --dry-run -avz --exclude "*.pyc" --exclude "vcms/storage" --exclude "*.mp4" --exclude "src/bin" . root@64.31.10.168:/vagrant
 
-#read confirmation
+echo 'Any key to proceed, or ctrl+c to cancel...'
+read confirmation
 
 rsync -avz --exclude "*.pyc" --exclude "vcms/storage" --exclude "*.mp4" --exclude "src/bin" . root@64.31.10.168:/vagrant
 
-ssh root@64.31.10.168 /etc/init.d/uwsgi restart # reload
+ssh root@64.31.10.168 /etc/init.d/uwsgi reload
 ssh root@64.31.10.168 /etc/init.d/memcached restart
 ssh root@64.31.10.168 python /vagrant/vcms/manage_lajornadavideos.py collectstatic --noinput
 ssh root@64.31.10.168 python /vagrant/vcms/manage_vcms.py collectstatic --noinput
