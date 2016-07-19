d12dadd75a3123879cc87a7f04f62495e1dddc65
David Regla <dreglad@gmail.com>
2016-07-19 07:20:12 -0500

    removed haystack config from backend settings

--- vcms/lajornadavideos/settings.py
+++ vcms/lajornadavideos/settings.py
@@ -212,59 +212,59 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-ELASTICSEARCH_INDEX_SETTINGS = {
-    'settings': {
-        "analysis": {
-            "analyzer": {
-                "synonym_analyzer" : {
-                    "type": "custom",
-                    "tokenizer" : "standard",
-                    "filter" : ["synonym"]
-                },
-                "ngram_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "lowercase",
-                    "filter": ["haystack_ngram", "synonym"]
-                },
-                "edgengram_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "lowercase",
-                    "filter": ["haystack_edgengram"]
-                }
-            },
-            "tokenizer": {
-                "haystack_ngram_tokenizer": {
-                    "type": "nGram",
-                    "min_gram": 3,
-                    "max_gram": 15,
-                },
-                "haystack_edgengram_tokenizer": {
-                    "type": "edgeNGram",
-                    "min_gram": 2,
-                    "max_gram": 15,
-                    "side": "front"
-                }
-            },
-            "filter": {
-                "haystack_ngram": {
-                    "type": "nGram",
-                    "min_gram": 3,
-                    "max_gram": 15
-                },
-                "haystack_edgengram": {
-                    "type": "edgeNGram",
-                    "min_gram": 2,
-                    "max_gram": 15
-                },
-                "synonym" : {
-                    "type" : "synonym",
-                    "ignore_case": "true",
-                    "synonyms_path" : "synonyms.txt"
-                }
-            }
-        }
-    }
-}
+# ELASTICSEARCH_INDEX_SETTINGS = {
+#     'settings': {
+#         "analysis": {
+#             "analyzer": {
+#                 "synonym_analyzer" : {
+#                     "type": "custom",
+#                     "tokenizer" : "standard",
+#                     "filter" : ["synonym"]
+#                 },
+#                 "ngram_analyzer": {
+#                     "type": "custom",
+#                     "tokenizer": "lowercase",
+#                     "filter": ["haystack_ngram", "synonym"]
+#                 },
+#                 "edgengram_analyzer": {
+#                     "type": "custom",
+#                     "tokenizer": "lowercase",
+#                     "filter": ["haystack_edgengram"]
+#                 }
+#             },
+#             "tokenizer": {
+#                 "haystack_ngram_tokenizer": {
+#                     "type": "nGram",
+#                     "min_gram": 3,
+#                     "max_gram": 15,
+#                 },
+#                 "haystack_edgengram_tokenizer": {
+#                     "type": "edgeNGram",
+#                     "min_gram": 2,
+#                     "max_gram": 15,
+#                     "side": "front"
+#                 }
+#             },
+#             "filter": {
+#                 "haystack_ngram": {
+#                     "type": "nGram",
+#                     "min_gram": 3,
+#                     "max_gram": 15
+#                 },
+#                 "haystack_edgengram": {
+#                     "type": "edgeNGram",
+#                     "min_gram": 2,
+#                     "max_gram": 15
+#                 },
+#                 "synonym" : {
+#                     "type" : "synonym",
+#                     "ignore_case": "true",
+#                     "synonyms_path" : "synonyms.txt"
+#                 }
+#             }
+#         }
+#     }
+# }
 
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
