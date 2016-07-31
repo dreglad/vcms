5da0b8482f4cab091cad62fe4836cde36b913d15
David Regla <dreglad@gmail.com>
2016-07-31 11:01:27 -0500

    added SeccionSitemap

--- vcms/lajornadavideos/sitemaps.py
+++ vcms/lajornadavideos/sitemaps.py
@@ -2,9 +2,24 @@ from django.contrib.sitemaps import Sitemap
 
 from videos.models import Video
 
+class SeccionSitemap(Sitemap):
+    changefreq = "never"
+    protocol = 'https'
+    priority = 0.5
+
+    def items(self):
+        return Pagina.objects.filter(activo=True)
+
+    def lastmod(self, obj):
+        return obj.fecha_modificacion or obj.fecha_creacion
+
+    def location(self, obj):
+        return obj.get_absolute_url(absolute=False)
+
+
 class VideoSitemap(Sitemap):
     changefreq = "never"
-    #protocol = 'https'
+    protocol = 'https'
     priority = 0.5
     limit = 100
 
